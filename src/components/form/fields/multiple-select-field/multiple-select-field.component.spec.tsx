678fa26b452a92265eb42d622619974d64a45831
Islom <nanotexnolagiya@gmail.com>
2022-06-04 23:10:30 +0500

    refactor: multiple select unit test add props

--- src/components/form/fields/multiple-select-field/multiple-select-field.component.spec.tsx
+++ src/components/form/fields/multiple-select-field/multiple-select-field.component.spec.tsx
@@ -1,10 +1,15 @@
 import { customRender } from '@test-utils';
+import { renderHook } from '@testing-library/react-hooks';
+import { useForm } from 'react-hook-form';
 import { MultipleSelectField } from './multiple-select-field.component';
 
 describe('MultipleSelectField', () => {
   test('Should render MultipleSelectField component', async () => {
+    const { result } = renderHook(() => useForm({
+      defaultValues: { test: '' }
+    }));
     const { asFragment } = customRender(
-      <MultipleSelectField />
+      <MultipleSelectField control={result.current.control} name='test' items={[]} />
     );
     expect(asFragment()).toMatchSnapshot();
   });
