c1995756d7c49ac72bddae87c7eac1aefc01e206
Islom <nanotexnolagiya@gmail.com>
2022-06-05 16:28:51 +0500

    refactor: switch field unit tests

--- src/components/form/fields/switch-field/switch-field.component.spec.tsx
+++ src/components/form/fields/switch-field/switch-field.component.spec.tsx
@@ -1,10 +1,15 @@
 import { customRender } from '@test-utils';
+import { renderHook } from '@testing-library/react-hooks';
+import { useForm } from 'react-hook-form';
 import { SwitchField } from './switch-field.component';
 
 describe('SwitchField', () => {
   test('Should render SwitchField component', async () => {
+    const { result } = renderHook(() => useForm({
+      defaultValues: { test: '' }
+    }));
     const { asFragment } = customRender(
-      <SwitchField />
+      <SwitchField name='test' control={result.current.control} label='Switch' />
     );
     expect(asFragment()).toMatchSnapshot();
   });
